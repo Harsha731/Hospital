532016ba60d1c1230b96dc0ffe7cdd31f9fbf580
harsha <harshapraneeth51@gmail.com>
2024-11-03 13:37:38 +0530

    build

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 const { connectDB } = require('./db');
 const docRoute = require("./routes/docRoute");
@@ -14,9 +15,15 @@ app.use(cors());
 app.use(bodyParser.json({extended:true,limit:'32mb'}));
 app.use(bodyParser.urlencoded({limit:'32mb',extended:true}));
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 connectDB();
 
 app.use("/api/docs",docRoute);
 app.use("/api/recept",receptRoute);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(3006,()=>console.log(`Running on Port 3006 `))
